d496cd34bbd225b239a8a20fd948595272499d0e
ruhulaminrul <ruhul.ok@gmail.com>
2019-10-26 08:46:05 +0600

    table schema

--- image CRUD simple laravel project/database/migrations/2019_10_26_024010_create_images_table.php
+++ image CRUD simple laravel project/database/migrations/2019_10_26_024010_create_images_table.php
@@ -15,7 +15,8 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('imageName');
+            $table->string('imageLocation');
         });
     }
 
