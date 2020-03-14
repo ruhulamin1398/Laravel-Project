471208532d3fc2391c7a8c7f94e1f70b1d27b9c3
ruhulaminrul <ruhul.ok@gmail.com>
2020-03-14 10:43:08 +0600

    initialize

---
+++ Guzzle Api /app/Http/Controllers/SmsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\sms;
+use Illuminate\Http\Request;
+
+class SmsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\sms  $sms
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sms $sms)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\sms  $sms
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sms $sms)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\sms  $sms
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sms $sms)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\sms  $sms
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sms $sms)
+    {
+        //
+    }
+}
